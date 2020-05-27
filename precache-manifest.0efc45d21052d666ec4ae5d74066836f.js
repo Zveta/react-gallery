28f67966d16c3f87ceceef81539a3f7aff0c4e5b
Zveta <svet.v.gorbunova@gmail.com>
2020-05-27 22:27:10 +0300

    Updates

---
+++ precache-manifest.0efc45d21052d666ec4ae5d74066836f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "77f3b7b26f41336eff9e9996a81ed117",
+    "url": "/react-gallery/static/media/RobotoCondensed.77f3b7b2.woff"
+  },
+  {
+    "revision": "168906784b9072156993eb34eb89ac02",
+    "url": "/react-gallery/static/media/RobotoCondensed.16890678.woff2"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/react-gallery/static/media/Roboto.ba3dcd89.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/react-gallery/static/media/Roboto.2751ee43.woff2"
+  },
+  {
+    "revision": "6a638fedae60f6c8df15",
+    "url": "/react-gallery/static/js/runtime~main.6a638fed.js"
+  },
+  {
+    "revision": "1e670679072d9d7870be",
+    "url": "/react-gallery/static/js/main.1e670679.chunk.js"
+  },
+  {
+    "revision": "aa3288456c47d69f2771",
+    "url": "/react-gallery/static/js/1.aa328845.chunk.js"
+  },
+  {
+    "revision": "1e670679072d9d7870be",
+    "url": "/react-gallery/static/css/main.c8199f63.chunk.css"
+  },
+  {
+    "revision": "0252a01aad27c87ebbbf701291a38b9d",
+    "url": "/react-gallery/index.html"
+  }
+];
